531a73f6521bee1d734e44c4df141cfd7ec72b14
GatlinNelson <gnelsondirect@outlook.com>
2021-11-06 13:48:51 -0800

    reorg

--- pi-init/raspi-setup.sh
+++ pi-init/raspi-setup.sh
@@ -1,38 +1,60 @@
 #!/bin/bash
+logfile=/home/$USER/init.log
+clear
 
-read -p "Master of Worker node? (m/w)> " nodetype
+echo "Logging to: $logfile"
+echo -e "\n\n"
+ip a s | grep eth0
+echo -e "\n\n"
+read -p "Master or Worker node? (m/w)> " nodetype
 
 if [ $nodetype = "m" ]
 then
     read -p "What is the new name of the master node?> " masname
+    echo $masname > /etc/hostname
 else
     read -p "What will this node's worker ID be? (Ex: a number or string of letters with no spaces)> " nodename
+    echo $nodename > /etc/hostname
 fi
 
-
 # Networking
-echo $nodename > /etc/hostname
+ip a s | grep eth0 >> $logfile
+echo -e "\n\n------------------------------\n\n" >> $logfile
 echo "nameserver 1.1.1.1" > /etvc/resolv.conf
 echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
-sudo apt update && sudo apt upgrade
-
-sudo apt install nmap git docker iperf3 speedtest-cli python3 python3-pip
-
+echo "Getting things up to date (update & upgrade)..."
+sudo apt update && sudo apt upgrade >> $logfile
+echo -e "\n\n------------------------------\n\n" >> $logfile
+echo "Installing 7 packages..."
+sudo apt install nmap git docker iperf3 speedtest-cli python3 python3-pip >> $logfile
+echo -e "\n\n------------------------------\n\n" >> $logfile
 chmod -R 777 .
 
 
-if [ $nodetype = "m" ]
+if [[ "$nodetype" = "m" ]];
 then
+    echo "Setting up ansible..."
     bash master/mas-ab.sh
+    echo "Setting up kubernetes..."
     bash master/mas-kb.sh
+    echo "Setting up kubeflow..."
     bash master/mas-kf.sh
+    echo "Setting up smb..."
     bash master/mas-smb.sh
-else
+else if [[ "$nodetype" = "w" ]];
+then
+    echo "Setting up ansible..."
     bash worker/work-ab.sh
+    echo "Setting up kubernetes..."
     bash worker/work-kb.sh
+    echo "Setting up kubeflow..."
     bash worker/work-kf.sh
+    echo "Setting up smb..."
     bash worker/work-smb.sh
+else
+    exit
 fi
 
+read -p "Setup done, would you like to reboot now? (Y/n)> 
 sudo reboot now
