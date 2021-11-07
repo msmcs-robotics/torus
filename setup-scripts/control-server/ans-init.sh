187dc174e9f30a691bffa02a4079df5e156f0f58
GatlinNelson <gnelsondirect@outlook.com>
2021-11-07 14:44:47 -0900

    update

--- setup-scripts/control-server/ans-init.sh
+++ setup-scripts/control-server/ans-init.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 clear
-echo -e "Setting up Ansible...\n\n"
+echo -e "Setting up Ansible...\n"
 # check to see if user would like to copy the contents of an existing file
 
 # create new hosts file and append info to
 # check if using the same creds across all nodes
 # for node in nodesarray, insert IP into hosts file
-mkdir ${HOME}/torus-setup-logs
-logfile=${HOME}/torus-setup-logs/ans-setup.log
+#mkdir ${HOME}/torus-setup-logs
+#logfile=${HOME}/torus-setup-logs/ans-setup.log
 clustename=$1
 clusteruser=$2
 clusterpass=$3
 outdir=$4
+if [ -z "$clustename" ]; then
+    clear
+    echo "Cluster has no name!!!"
+    exit
+fi
+if [ -z "$clusteruser" ]; then
+    clear
+    echo "Cluster has no user!!!"
+    exit
+fi
+if [ -z "$clusterpass" ]; then
+    clear
+    echo "Cluster has no password!!!"
+    exit
+fi
+if [ -z "$outdir" ]; then
+    clear
+    echo "hosts file has no destination!!!"
+    exit
+fi
 
 ###################     HOSTS FILE     ###################
 out="${outdir}/hosts"
 touch ${out}
 echo "[$clustename]" > ${out}
-echo "Enter each node address on a new line, to stop, leave line empty."
+echo -e "Enter each node address on a new line, to stop, leave line empty.\n"
 i=1
 while :
 do
