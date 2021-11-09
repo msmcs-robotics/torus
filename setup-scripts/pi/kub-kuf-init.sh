3600723c7d376bfac2ff55e23dd3792cb24c5c70
GatlinNelson <gnelsondirect@outlook.com>
2021-11-09 13:04:55 -0900

    update

--- setup-scripts/pi/kub-kuf-init.sh
+++ setup-scripts/pi/kub-kuf-init.sh
@@ -72,15 +72,9 @@ after_reboot(){
         sudo cat /var/lib/rancher/k3s/server/node-token 2>&1 | tee -a $tokenfile | tee -a $logfile
         echo -e "\n\n------------------------------\n\n" 2>&1 | tee -a $logfile
 
-        ##########      KubeFlow    ##########
-        export PIPELINE_VERSION=1.7.1
-        kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION" 2>&1 | tee -a $logfile
-        echo -e "\n\n------------------------------\n\n" 2>&1 | tee -a $logfile
-        kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io 2>&1 | tee -a $logfile
-        echo -e "\n\n------------------------------\n\n" 2>&1 | tee -a $logfile
-        kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic-pns?ref=$PIPELINE_VERSION" 2>&1 | tee -a $logfile
-        echo -e "\n\n------------------------------\n\n" 2>&1 | tee -a $logfile
-        #kubectl port-forward -n kubeflow svc/ml-pipeline-ui 8080:80
+        sudo mkdir /tasks
+        sudo chmod 777 /tasks
+        echo -e "Created tasks directory..."
 
     elif [ $nodetype = "w" ]; then
         echo "ok..."
